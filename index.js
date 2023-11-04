52eec517964183431ab78ffc984fa99b8a889431
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2023-11-04 21:43:26 +0530

    just created the structure for the climaLite

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ import bodyParser from "body-parser";
 import express from "express";
 
 // code 
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+});
