c6ea51c7a332d6b19b984b443a4e4293001a69b2
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2023-11-05 22:30:28 +0530

    tested image response with ejs file

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const Api_Key='24ac61e2a7cad920904fa9d632d60657';
+const loc="thrissur";
 
 
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.get("/",async (req,res)=>{
+    const result =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${Api_Key}`);
+    const icon=result.data.weather[0].icon.slice(0,2);
+    console.log(icon);
+    res.render("index.ejs",{iconData:icon});
 });
 
 app.listen(port,()=>{
