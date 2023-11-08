1f7d952d7211b35927ae02958d7912d6ba18f01f
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2023-11-08 19:41:40 +0530

    fetched required data from the APIand passed to Ejs file

--- index.js
+++ index.js
@@ -16,14 +16,34 @@ app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
 
+
+
 app.post("/forecast",async(req,res)=>
 {
     const loc=req.body.city;
-    console.log(loc);
     var result =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${Api_Key}`);
     var icon=result.data.weather[0].icon.slice(0,2);
-    console.log(icon);
-    res.render("index.ejs",{iconData:icon});
+    var description=result.data.weather[0].description;
+    var celcius=(result.data.main.temp-273).toFixed(2);
+    var minTemp=(result.data.main.temp_min-273).toFixed(2);
+    var maxTemp=(result.data.main.temp_max-273).toFixed(2);
+    var feelsLike=(result.data.main.feels_like-273).toFixed(2);
+    var humidity=result.data.main.humidity;
+    var pressure=result.data.main.pressure;
+    var windSpeed=result.data.wind.speed;
+    console.log(windSpeed)
+    res.render("index.ejs",
+    {
+        iconData:icon,
+        descriptionData:description,
+        celciusData:celcius,
+        minTempData:minTemp,
+        maxTempData:maxTemp,
+        feelsLikeData:feelsLike,
+        humidityData:humidity,
+        pressureData:pressure,
+        windSpeedData:windSpeed,
+    });
 });
 
 app.listen(port,()=>{
