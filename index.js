100050666774e5280176e428d91beba0d2cad336
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2023-11-08 18:37:36 +0530

    Done initial post req

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 const Api_Key='24ac61e2a7cad920904fa9d632d60657';
-const loc="thrissur";
 
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",async (req,res)=>{
-    const result =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${Api_Key}`);
-    const icon=result.data.weather[0].icon.slice(0,2);
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/forecast",async(req,res)=>
+{
+    const loc=req.body.city;
+    console.log(loc);
+    var result =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${Api_Key}`);
+    var icon=result.data.weather[0].icon.slice(0,2);
     console.log(icon);
     res.render("index.ejs",{iconData:icon});
 });
